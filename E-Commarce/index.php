e2ec4cb2cfc56fc7bc32718ef1997f43b917e4d3
Asma Akter <143210807+asma-akter77@users.noreply.github.com>
2023-09-04 16:55:24 +0600

    upload first time

---
+++ E-Commarce/index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- style css -->
+    <link rel="stylesheet" href="style.css">
+    <!-- botstrap css -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+     <!-- fonts aswome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+     <!-- Title -->
+    <title>Welcome</title>
+</head>
+
+
+<body>
+    <div class="container-fluid p-0">
+
+        <!-- First Child Nav bar -->
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <img class="logo" src="./images/logo.jpg" alt="Your Company Logo">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Product</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Register</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fa-solid fa-cart-shopping"></i><sup>1</sup></a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Total Price:100/-</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+                </div>
+            </div>
+        </nav>
+
+
+        <!-- second child log in geust-->
+        <div class="navbar navbar-expand-lg bg-info">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                        <a class="nav-link" href="#">Welcome guest</a>
+                </li>
+                <li class="nav-item">
+                        <a class="nav-link" href="#">Log in</a>
+                </li>
+                <li class="nav-item">
+                        <a class="nav-link" href="#">Register</a>
+                </li>
+            </ul>
+        </div>
+
+
+         <!-- Third child-->
+         <div class="bg-light text-center">
+            <h1>Online Store</h1>
+            <p> -- Enjoy shopping with best price -- </p>
+         </div>
+
+
+         <!-- Fourth child-->
+         <div class="row">
+            <!-- product card-->
+            <div class="col-md-10">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card" style="width: 18rem;">
+                            <img src="https://asia-exstatic-vivofs.vivo.com/PSee2l50xoirPK7y/1677571104137/afee365e27ec71ce13a2b6664ecde107.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Mobile</h5>
+                                <p class="card-text">Godd product good valo</p>
+                                <a href="#" class="btn btn-primary">ADD TO CART</a>
+                                <a href="#" class="btn btn-secondary">View details</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card" style="width: 18rem;">
+                            <img src="https://asia-exstatic-vivofs.vivo.com/PSee2l50xoirPK7y/1677571104137/afee365e27ec71ce13a2b6664ecde107.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Mobile</h5>
+                                <p class="card-text">Godd product good valo</p>
+                                <a href="#" class="btn btn-primary">ADD TO CART</a>
+                                <a href="#" class="btn btn-secondary">View details</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card" style="width: 18rem;">
+                            <img src="https://asia-exstatic-vivofs.vivo.com/PSee2l50xoirPK7y/1677571104137/afee365e27ec71ce13a2b6664ecde107.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Mobile</h5>
+                                <p class="card-text">Godd product good valo</p>
+                                
+                                <a href="#" class="btn btn-primary">ADD TO CART</a>
+                                <a href="#" class="btn btn-secondary">View details</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <!-- sidenav -->
+            <div class="col-md-2 bg-dark">
+
+            </div>
+         </div>
+
+
+
+
+
+
+
+
+
+        <!--Last child-->
+        <div class="bg-body-tertiary p-3 text-center">
+            <p>Copyright © 2023 - Istiak Tanvir®. All rights reserved.</p>
+        </div>
+
+
+   
+    </div>
+
+    <!-- botstrap js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
+</body>
+</html>
